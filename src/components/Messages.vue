4d0f7725c00cfeed2b7adcfadbc2b03b084c501d
CrimsonMax <61148415+CrimsonMax@users.noreply.github.com>
2021-06-27 12:32:23 +0300

    pagination success

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="wrapper">
+  <div class="wrapper" v-on:scroll="pagination">
     <div class="messages">
       <div v-for="message in messages" :key="message.date" class="post">
         <div class="post-header">
-          <span>{{ message.date }}</span> /
-          <span style="font-weight: bold">{{ message.authorName }}</span> /
+          <span>{{
+            new Date(message.date).toLocaleString("ru", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+              timezone: "UTC",
+            })
+          }}</span>
+          / <span style="font-weight: bold">{{ message.authorName }}</span> /
           <a href="{{message.authorUrl}}">{{ message.authorUrl }}</a>
         </div>
         <div class="post-text">
           {{ message.content }}
         </div>
         <div class="post-line"></div>
+      </div>
+      <div class="lds-roller" v-show="!loader">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     </div>
   </div>
@@ -24,9 +41,10 @@ export default {
     return {
       messages: [],
       num: 10,
+      loader: false,
     };
   },
-  async created() {
+  async beforeMount() {
     try {
       const info = await axios.get(
         `http://localhost:3000/messages?_limit=${this.num}`
@@ -36,29 +54,24 @@ export default {
       console.log(e);
     }
   },
-  // methods: {
-    // v-on:scroll="pagination"
-  //   pagination: function () {
-  //     console.log("scrollTop", document.querySelector(".wrapper").scrollTop);
-  //     console.log(
-  //       "scrollHeight",
-  //       document.querySelector(".wrapper").scrollHeight
-  //     );
-  //     if (
-  //       document.querySelector(".wrapper").scrollTop +
-  //         document.querySelector(".wrapper").offsetHeight ===
-  //       document.querySelector(".wrapper").scrollHeight
-  //     ) {
-  //       this.num += 10;
-  //       debugger;
-  //       console.log(this.num);
-  //       const NewInfo = axios.get(
-  //         `http://localhost:3000/messages?_limit=${this.num}`
-  //       );
-  //       this.messages = NewInfo.data;
-  //     }
-  //   },
-  // },
+  methods: {
+    pagination: function () {
+      if (
+        document.querySelector(".wrapper").scrollTop +
+          document.querySelector(".wrapper").offsetHeight ===
+        document.querySelector(".wrapper").scrollHeight
+      ) {
+        this.loader = true;
+        this.num += 10;
+        axios
+          .get(`http://localhost:3000/messages?_limit=${this.num}`)
+          .then((res) => (this.messages = res.data));
+        setTimeout(() => {
+          this.loader = false;
+        }, 1000);
+      }
+    },
+  },
 };
 </script>
 
@@ -70,10 +83,16 @@ export default {
   box-shadow: 4px 4px 8px 0px rgb(34 60 80 / 40%);
   overflow-y: scroll;
   background-color: floralwhite;
+  /* display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center; */
 }
 
 .messages {
   height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 .post {
@@ -124,5 +143,96 @@ export default {
 
 ::-webkit-scrollbar {
   width: 4px;
+}
+
+/*===================
+  Loader
+===================*/
+
+.lds-roller {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+  margin: 0 auto;
+  align-self: center;
+}
+.lds-roller div {
+  animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  transform-origin: 40px 40px;
+}
+.lds-roller div:after {
+  content: " ";
+  display: block;
+  position: absolute;
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: rgb(165, 16, 16);
+  margin: -4px 0 0 -4px;
+}
+.lds-roller div:nth-child(1) {
+  animation-delay: -0.036s;
+}
+.lds-roller div:nth-child(1):after {
+  top: 63px;
+  left: 63px;
+}
+.lds-roller div:nth-child(2) {
+  animation-delay: -0.072s;
+}
+.lds-roller div:nth-child(2):after {
+  top: 68px;
+  left: 56px;
+}
+.lds-roller div:nth-child(3) {
+  animation-delay: -0.108s;
+}
+.lds-roller div:nth-child(3):after {
+  top: 71px;
+  left: 48px;
+}
+.lds-roller div:nth-child(4) {
+  animation-delay: -0.144s;
+}
+.lds-roller div:nth-child(4):after {
+  top: 72px;
+  left: 40px;
+}
+.lds-roller div:nth-child(5) {
+  animation-delay: -0.18s;
+}
+.lds-roller div:nth-child(5):after {
+  top: 71px;
+  left: 32px;
+}
+.lds-roller div:nth-child(6) {
+  animation-delay: -0.216s;
+}
+.lds-roller div:nth-child(6):after {
+  top: 68px;
+  left: 24px;
+}
+.lds-roller div:nth-child(7) {
+  animation-delay: -0.252s;
+}
+.lds-roller div:nth-child(7):after {
+  top: 63px;
+  left: 17px;
+}
+.lds-roller div:nth-child(8) {
+  animation-delay: -0.288s;
+}
+.lds-roller div:nth-child(8):after {
+  top: 56px;
+  left: 12px;
+}
+@keyframes lds-roller {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
